3b5cb375d7b9866235b217081892f85783ed7bc2
BaekChanwoo <bcw043785@gmail.com>
2022-09-19 11:04:59 +0900

    Initial Commit

---
+++ photoApp/photoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoAppApp.swift
+//  photoApp
+//
+//  Created by 백찬우 on 2022/09/19.
+//
+
+import SwiftUI
+
+@main
+struct photoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
